3a7305c58a0ace4b68d3093fe2fe4630fa66a03e
Divyansh <divyansh.nankani@gmail.com>
2026-07-13 17:21:51 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,125 @@
+use bevy::prelude::*;
+
+use crate::player::{Player, PlayerPlugin};
+mod player;
+
+// COMPONENT
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    // spawning entities
+    commands.spawn((Person, Name("Rudolf Rhodie".to_string())));
+    commands.spawn((Person, Name("Yulu Tulu".to_string())));
+    commands.spawn((Person, Name("Halo Holo".to_string())));
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Yulu Tulu" {
+            name.0 = "Yulu Gulu".to_string();
+        }
+    }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+// SYSTEMS
+fn print_position(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {}", position.x, position.y);
+    }
+}
+
+// ENTITIES
+struct Entity(u64);
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        // adding things to App here
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        // .add_systems(Update, hello_world);
+    }
+}
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::WHITE))
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            file_path: "src/assets".into(),
+            ..default()
+        }))
+        .add_systems(Startup, setup_camera)
+        .add_plugins(PlayerPlugin) // Update this line
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Text2d::new("()"),
+        TextFont {
+            font_size: FontSize::Px(12.0),
+            font: default(),
+            ..default()
+        },
+        TextColor(Color::WHITE),
+        Transform::from_translation(Vec3::ZERO),
+        Player,
+    ));
+}
+
+fn move_player(
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut player_transform: Single<&mut Transform, With<Player>>,
+) {
+    let mut direction = Vec2::ZERO;
+    if input.pressed(KeyCode::ArrowLeft) {
+        direction.x -= 1.0;
+    }
+    if input.pressed(KeyCode::ArrowRight) {
+        direction.x += 1.0;
+    }
+    if input.pressed(KeyCode::ArrowUp) {
+        direction.y -= 1.0;
+    }
+    if input.pressed(KeyCode::ArrowDown) {
+        direction.y += 1.0;
+    }
+
+    if direction != Vec2::ZERO {
+        let speed = 300.0;
+        let delta = direction.normalize() * speed * time.delta_secs();
+        player_transform.translation.x += delta.x;
+        player_transform.translation.y += delta.y;
+    }
+}
